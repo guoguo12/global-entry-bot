c43127350d08f4a79cf34e35b3afc10590548695
Christopher González Wienberg <christopher@gonzberg.net>
2022-08-26 23:42:38 -0700

    Refactor message creation and tweeting into a class.
    This will make it easier to handle secrets from ENV or file.

--- main.py
+++ main.py
@@ -11,6 +11,13 @@ from secrets import twitter_credentials
 
 LOGGING_FORMAT = '%(asctime)s %(name)-12s %(levelname)-8s %(message)s'
 
+DELTA_WEEKS = 4
+
+SCHEDULER_API_URL = 'https://ttp.cbp.dhs.gov/schedulerapi/locations/{location}/slots?startTimestamp={start}&endTimestamp={end}'
+TTP_TIME_FORMAT = '%Y-%m-%dT%H:%M'
+
+NOTIF_MESSAGE = 'New appointment slot open at {location}: {date}'
+MESSAGE_TIME_FORMAT = '%A, %B %d, %Y at %I:%M %p'
 
 class Location(NamedTuple):
     name: str
@@ -22,23 +29,33 @@ class Location(NamedTuple):
         return Location(name, int(code))
 
 
-DELTA_WEEKS = 4
+class AppointmentTweeter(object):
 
-SCHEDULER_API_URL = 'https://ttp.cbp.dhs.gov/schedulerapi/locations/{location}/slots?startTimestamp={start}&endTimestamp={end}'
-TTP_TIME_FORMAT = '%Y-%m-%dT%H:%M'
+    def __init__(self, test_mode, consumer_key, consumer_secret, access_token_key, access_token_secret):
+        self._test_mode = test_mode
+        self._api = twitter.Api(
+            consumer_key=consumer_key, consumer_secret=consumer_secret,
+            access_token_key=access_token_key, access_token_secret=access_token_secret
+        )
 
-NOTIF_MESSAGE = 'New appointment slot open at {location}: {date}'
-MESSAGE_TIME_FORMAT = '%A, %B %d, %Y at %I:%M %p'
+    def tweet(self, location_name, localized_time):
+        timestamp = datetime.strptime(localized_time, TTP_TIME_FORMAT)
+        message = NOTIF_MESSAGE.format(location=location_name,
+                                        date=timestamp.strftime(MESSAGE_TIME_FORMAT))
+        logging.info('Message: ' + message)
+        if not self._test_mode:
+            self._tweet(message)
 
-def tweet(message):
-    api = twitter.Api(**twitter_credentials)
-    try:
-        api.PostUpdate(message)
-    except twitter.TwitterError as e:
-        if len(e.message) == 1 and e.message[0]['code'] == 187:
-            logging.info('Tweet rejected (duplicate status)')
-        else:
-            raise
+    def _tweet(self, message):
+        logging.info('Tweeting: ' + message)
+        try:
+            self._api.PostUpdate(message)
+        except twitter.TwitterError as e:
+            if len(e.message) == 1 and e.message[0]['code'] == 187:
+                logging.info('Tweet rejected (duplicate status)')
+            else:
+                raise
+
 
 def check_for_openings(location_name, location_code, test_mode=True):
     start = datetime.now()
@@ -53,18 +70,13 @@ def check_for_openings(location_name, location_code, test_mode=True):
         logging.exception('Could not connect to scheduler API')
         sys.exit(1)
 
+    appointment_tweeter = AppointmentTweeter(test_mode=test_mode, **twitter_credentials)
+
     for result in results:
         if result['active'] > 0:
             logging.info('Opening found for {}'.format(location_name))
 
-            timestamp = datetime.strptime(result['timestamp'], TTP_TIME_FORMAT)
-            message = NOTIF_MESSAGE.format(location=location_name,
-                                           date=timestamp.strftime(MESSAGE_TIME_FORMAT))
-            if test_mode:
-                print(message)
-            else:
-                logging.info('Tweeting: ' + message)
-                tweet(message)
+            appointment_tweeter.tweet(location_name, result['timestamp'])
             return  # Halt on first match
 
     logging.info('No openings for {}'.format(location_name))
